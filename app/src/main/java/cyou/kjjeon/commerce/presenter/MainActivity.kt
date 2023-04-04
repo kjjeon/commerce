df9461b67c03c1c0b1fb4bafa693919da6d78355
조엘_전국진 <joel@afreecatv.com>
2023-04-04 22:26:04 +0900

    init

---
+++ app/src/main/java/cyou/kjjeon/commerce/presenter/MainActivity.kt
@@ -0,0 +1,40 @@
+package cyou.kjjeon.commerce.presenter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cyou.kjjeon.commerce.presenter.ui.theme.CommerceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CommerceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CommerceTheme {
+        Greeting("Android")
+    }
+}
